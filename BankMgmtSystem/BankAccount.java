18d6d4ac3ce4f207e959c94081b36b14b2f5ef8f
Rutuja Shewale <rutujashewale16398@gmail.com>
2022-10-12 20:08:06 +0530

    Basic functionalities for bank

---
+++ BankMgmtSystem/BankAccount.java
@@ -0,0 +1,62 @@
+import java.util.*;
+class BankAccount{
+	private List<String> transactions;
+	
+	
+	private int previousTransaction,balance;
+	private String customerName,customerId;
+	
+	
+	 BankAccount(){
+	}
+	BankAccount(int balance,int previousTransaction,String customerId, String customerName,List<String> transactions){
+		this.balance=balance;
+		this.previousTransaction=previousTransaction;
+		this.customerId=customerId;
+		this.customerName=customerName;
+		this.transactions=transactions;
+	}
+	
+	public int getBalance(){
+		return this.balance;
+	}
+	
+	public int getPreviousTransaction(){
+		return this.previousTransaction;
+	}
+	
+	public String getCustomerId(){
+		return this.customerId;
+	}
+	
+	public String getCustomerName(){
+		return this.customerName;
+	}
+	
+	public void setBalance(int balance){
+		this.balance=balance;
+	}
+	
+	public void setPreviousTransaction(int previousTransaction){
+		this.previousTransaction=previousTransaction;
+	}
+	public void setCustomerId(String customerId){
+		this.customerId=customerId;
+	}
+	public void setCustomerName(String customerName){
+		this.customerName=customerName;
+	}
+	public List<String> getTransactions(){
+		return this.transactions;
+	}
+	public void setTransactions(List<String> transactions){
+		this.transactions=transactions;
+	}
+	
+	
+	
+	
+	public String toString(){
+		return this.balance + " "  + this.previousTransaction + " " + this.customerId + " " + this.customerName + " " +this.transactions;
+	}
+}
