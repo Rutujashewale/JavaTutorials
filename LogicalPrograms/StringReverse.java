93abc768a85a00c31b7505c0ab3455c8ef75cd9b
Rutuja Shewale <rutujashewale16398@gmail.com>
2022-11-09 00:45:21 +0530

    Logical program

--- LogicalPrograms/StringReverse.java
+++ LogicalPrograms/StringReverse.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
 class StringReverse{
-	public static void main(String args[]);
+	public static void main(String args[]){
+	boolean flag=false;
+	Scanner s=new Scanner(System.in);
 	
-	String str="Rutuja";
-	String rev=" ";
+	System.out.println("Enter a string to check palindrome or not");
+	String str=s.next();
 	
-	for(int i=0;i<str.length/2;i++){
-		
+	for(int i=0;i<str.length()/2;i++){
+		System.out.println(i);
+		if(str.charAt(i)!=str.charAt(str.length()-i-1)){
+			System.out.println("String not palindrome");
+			flag=true;
+			break;
+		}
 	}
+		if(flag==false){
+			System.out.println("String is palindrome");
+		}
+	}
+	
 }
